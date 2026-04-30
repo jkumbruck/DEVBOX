200cb207cff8fde02f998a0846bd4ab595049085
jkumbruck <101322183+jkumbruck@users.noreply.github.com>
2026-04-30 13:24:38 +0200

    Update wingetDSC_USER.ps1

--- wingetDSC_USER.ps1
+++ wingetDSC_USER.ps1
@@ -1,8 +1,10 @@
-Install-PackageProvider -Name NuGet -Force
-Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery
-Repair-WinGetPackageManager
-Install-Module Microsoft.WinGet.DSC -Force -Repository PSGallery
-winget configure --enable
+#Install-PackageProvider -Name NuGet -Force
+#Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery
+#Repair-WinGetPackageManager
+#Install-Module Microsoft.WinGet.DSC -Force -Repository PSGallery
+#winget configure --enable
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile "${env:temp}\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+Add-AppxPackage -Path "${env:temp}\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 winget configure --file "C:\Windows\Temp\devbox\DEPSIT\joeri.yaml" --accept-configuration-agreements
 Clear-Host
 
