75cd2e9f65585724ccc2689ec73659356140b62b
jkumbruck <101322183+jkumbruck@users.noreply.github.com>
2026-04-30 12:03:17 +0200

    Update wingetDSC_USER.ps1

--- wingetDSC_USER.ps1
+++ wingetDSC_USER.ps1
@@ -2,7 +2,7 @@
 #Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery
 #Repair-WinGetPackageManager
 #Install-Module Microsoft.WinGet.DSC -Force -Repository PSGallery
-winget configure -enable
+winget configure --enable
 winget configure --file "C:\Windows\Temp\devbox\DEPSIT\joeri.yaml" --accept-configuration-agreements
 Clear-Host
 
