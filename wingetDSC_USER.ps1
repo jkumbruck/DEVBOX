711d8923f7eeb0e974e878fd041dbed524fb668a
jkumbruck <101322183+jkumbruck@users.noreply.github.com>
2026-04-30 23:45:37 +0200

    Update wingetDSC_USER.ps1

--- wingetDSC_USER.ps1
+++ wingetDSC_USER.ps1
@@ -5,6 +5,14 @@
 #winget configure --enable
 #Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile "${env:temp}\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 Add-AppxPackage -Path "C:\Windows\Temp\WinGet-Stage\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+# Wait until the package appears in Get-AppxPackage
+while (-not (Get-AppxPackage -Name "Microsoft.DesktopAppInstaller")) {
+    Start-Sleep -Seconds 1
+}
+# Wait for AppX Deployment Server to finish processing
+while (Get-Process -Name AppXDeploymentServer -ErrorAction SilentlyContinue) {
+    Start-Sleep -Seconds 1
+}
 winget configure --file "C:\Windows\Temp\devbox\DEPSIT\joeri.yaml" --accept-configuration-agreements
 Clear-Host
 
